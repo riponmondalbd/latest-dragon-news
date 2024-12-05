e985893ddfcab336565d392cc9f676ec51f952e4
riponmondalbd <riponmondal149@gmail.com>
2024-12-06 04:18:15 +0600

    complete right side nav

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -2,6 +2,7 @@ import BreakingNews from "../BreakingNews";
 import Header from "../shared/Header/Header";
 import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
 import Navbar from "../shared/Navbar/Navbar";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
 
 const Home = () => {
   return (
@@ -9,17 +10,17 @@ const Home = () => {
       <Header />
       <BreakingNews />
       <Navbar />
-      <div className="grid md:grid-cols-4 gap-6 mt-[76px]">
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-6 mt-[76px]">
         <div>
           <LeftSideNav />
         </div>
-        <div className="md:col-span-2 ">
+        <div className="lg:col-span-2 ">
           <p className="text-xl font-semibold text-[#403F3F]">
             Dragon News Home
           </p>
         </div>
         <div>
-          <p className="text-xl font-semibold text-[#403F3F]">Login With</p>
+          <RightSideNav />
         </div>
       </div>
     </div>
---
+++ src/page/shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,78 @@
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import {
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoLogoTwitter,
+} from "react-icons/io5";
+import { Link } from "react-router-dom";
+import bg from "../../../assets/bg.png";
+import qZone1 from "../../../assets/qZone1.png";
+import qZone2 from "../../../assets/qZone2.png";
+import qZone3 from "../../../assets/qZone3.png";
+
+const RightSideNav = () => {
+  return (
+    <div>
+      {/* google twitter login */}
+      <div>
+        <p className="text-xl font-semibold text-[#403F3F] mb-5">Login With</p>
+        <button className="btn w-full mb-2">
+          <FaGoogle />
+          Google
+        </button>
+        <button className="btn w-full">
+          <FaGithub />
+          Github
+        </button>
+      </div>
+
+      {/* find us section */}
+      <div className="mt-5">
+        <p className="text-xl font-semibold text-[#403F3F]">Find Us On</p>
+        <div className="border divide-y rounded-t-lg rounded-b-lg my-5">
+          <div className="p-[17px] gap-[10px] flex items-center">
+            <IoLogoFacebook className="text-[30px]" />
+            <p className="font-medium text-[#706F6F]">Facebook</p>
+          </div>
+          <div className="p-[17px] gap-[10px] flex items-center">
+            <IoLogoInstagram className="text-[30px]" />
+            <p className="font-medium text-[#706F6F]">Instagram</p>
+          </div>
+          <div className="p-[17px] gap-[10px] flex items-center">
+            <IoLogoTwitter className="text-[30px]" />
+            <p className="font-medium text-[#706F6F]">Twitter</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Q-zone section */}
+      <div className="bg-[#F3F3F3] mb-5">
+        <p className="text-xl font-semibold text-[#403F3F] py-5 pl-[15px]">
+          Q-Zone
+        </p>
+        <img className="w-full" src={qZone1} alt="qZone1" />
+        <img className="w-full" src={qZone2} alt="qZone2" />
+        <img className="w-full" src={qZone3} alt="qZone3" />
+      </div>
+
+      {/* amazing newspaper section */}
+      <div
+        className="h-auto bg-cover bg-center"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="text-white text-center px-[38px] py-[65px]">
+          <h2 className="text-3xl font-bold">Create an Amazing Newspaper</h2>
+          <p className="font-normal mt-5">
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <Link className="text-xl font-semibold p-[18px] bg-[#D72050]">
+            <button className="mt-10">Learn More</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightSideNav;
