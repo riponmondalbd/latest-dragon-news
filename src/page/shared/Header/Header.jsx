8ade5755e81d4d36558f1cc3cb4eab305ca6e725
riponmondalbd <riponmondal149@gmail.com>
2024-12-05 23:13:59 +0600

    add date and time using moment js

--- src/page/shared/Header/Header.jsx
+++ src/page/shared/Header/Header.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 
@@ -9,6 +10,9 @@ const Header = () => {
       </Link>
       <p className="text-lg text-[#706F6F] font-normal mt-5 mb-[10px]">
         Journalism Without Fear or Favour
+      </p>
+      <p className="text-xl font-medium">
+        {moment().format("dddd, MMMM DD, YYYY")}
       </p>
     </div>
   );
